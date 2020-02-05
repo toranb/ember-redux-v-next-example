3dd943fb00c540cc3546833239991e95487e368e
toran billups <toranb@gmail.com>
2020-02-04 19:06:14 -0600

    upgrade to ember-redux vNext

---
+++ app/components/baz-baz.js
@@ -0,0 +1,40 @@
+import Component from '@glimmer/component';
+import { action, computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { connect } from 'ember-redux';
+
+const stateToComputed = function(state, attrs) {
+  return {
+    number: state.fooz,
+    greeting: `Hello ${attrs.name}!`
+  };
+};
+
+const dispatchToActions = (dispatch) => {
+  return {
+    up: () => dispatch({type: 'UP'})
+  };
+};
+
+class MyClazz extends Component {
+  @tracked color = 'red';
+
+  @computed('greeting')
+  get bar() {
+    const someKey = this.greeting;
+    return `${someKey} - bazbaz style`;
+  }
+
+  @action
+  go() {
+    window.console.log('Glimmer clicked baz baz yo!');
+    this.actions.up();
+  }
+
+  constructor() {
+    super(...arguments);
+    this.color = 'green';
+  }
+}
+
+export default connect(stateToComputed, dispatchToActions)(MyClazz);
